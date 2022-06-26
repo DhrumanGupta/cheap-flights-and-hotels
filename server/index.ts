ac7d5b05c2941c6d07aa580d3bfd9caf6a1934be
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-06-26 13:38:28 +0530

    rip

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express, { Express, Request, Response, NextFunction } from 'express'
-import { PrismaClient } from '@prisma/client'
 import cors from 'cors'
 import authController from './controllers/auth'
 import cookieParser from 'cookie-parser'
+import runWorkerTask from './worker'
+import cron from 'node-cron'
 
 const app: Express = express()
-const prisma = new PrismaClient()
 
 const port = 1337
 
@@ -18,7 +18,7 @@ app.use(
     cors({
         origin:
             prodEnv === 'development'
-                ? 'http://localhost:1337'
+                ? `http://localhost:${port}`
                 : 'https://backend.travelcheapwith.tech',
         credentials: true,
     })
@@ -35,3 +35,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server is running at https://localhost:${port}`)
 })
+
+cron.schedule('*/5 0 * * *', async () => await runWorkerTask())
--- server/worker.ts
+++ server/worker.ts
@@ -1,6 +1,13 @@
-import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
 
-dotenv.config();
+dotenv.config()
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
+
+const worker = async () => {
+    const links = await prisma.requests.findMany({ select: { url: true } })
+    const urls: string[] = links.map((x) => x.url)
+}
+
+export default worker
